6ae950f10bb4bddb75bad4b4d323743282e6c601
Muh11 <naufalaswinana@gmail.com>
2025-06-10 20:27:42 +0700

    feat: add navigation to plant widget

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -3,6 +3,7 @@ import 'package:page_transition/page_transition.dart';
 import 'package:plants_app/constants.dart';
 import 'package:plants_app/model/plants.dart';
 import 'package:plants_app/ui/screens/detail_page.dart';
+import 'package:plants_app/ui/screens/widget/plant_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -232,78 +233,8 @@ class _HomePageState extends State<HomePage> {
                 scrollDirection: Axis.vertical,
                 physics: const BouncingScrollPhysics(),
                 itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                    onTap:(){
-                      Navigator.push(context, PageTransition(child: DetailPage(
-                        plantId: _plantList[index].plantId),
-                      type: PageTransitionType.bottomToTop
-                      ));
-                    },
-                    child: Container(
-                      decoration:BoxDecoration(
-                        color: Constants.primaryColor.withOpacity(.1),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      height: 80.0,
-                      padding: const EdgeInsets.only(left: 10, top: 10),
-                      margin: const EdgeInsets.only(bottom: 10, top: 10),
-                      width: size.width,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Stack(
-                            clipBehavior: Clip.none,
-                            children: [
-                              Container(
-                                width: 60.0,
-                                height: 60.0,
-                                decoration: BoxDecoration(
-                                  color: Constants.primaryColor.withOpacity(.8),
-                                  shape: BoxShape.circle
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 5,
-                                left: 0,
-                                right: 0,
-                                child: SizedBox(
-                                  height: 80.0,
-                                  child: Image.asset(_plantList[index].imageURL),
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 5,
-                                left: 80,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(_plantList[index].category),
-                                    Text(_plantList[index].plantName, style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
-                                      color: Constants.blackColor,
-                                    ))
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                          Container(
-                            padding: const EdgeInsets.only(right: 10),
-                            child: Text(
-                              r'$' + _plantList[index].price.toString(), style: 
-                              TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18.0,
-                                color: Constants.primaryColor,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  );
+                  return PlantWidget(index : index, plantList: _plantList);
+                  
                 },
               ),
             ),
@@ -313,3 +244,4 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
